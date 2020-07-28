6c5d197ef989543332b6aff7914fd72d27ffbcd4
emadum <eric.adum@mongodb.com>
2020-07-27 23:45:14 -0400

    add node v4 and s390x platform

--- .evergreen/run-prebuild.sh
+++ .evergreen/run-prebuild.sh
@@ -14,6 +14,7 @@ run_prebuild() {
   else
     echo "Github token detected. Running prebuild."
     npm run prebuild -- -u $NODE_GITHUB_TOKEN
+    npm run prebuild-legacy -- -u $NODE_GITHUB_TOKEN
     echo "Prebuild's successfully submitted"
   fi
 }
