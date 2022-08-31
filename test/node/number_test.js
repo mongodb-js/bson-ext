6b20234e430d35ef0e972c634e700f6abbe61a16
Neal Beeken <neal.beeken@mongodb.com>
2022-08-31 17:22:35 -0400

    demonstrate Double class preserving neg zero

--- test/node/number_test.js
+++ test/node/number_test.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
-const BSON = require('../..');
+const BSON = require("../..");
 
 const BSON_DOUBLE_TYPE = 0x01;
 const BSON_INT32_TYPE = 0x10;
@@ -10,7 +10,7 @@ const BSON_INT32_TYPE = 0x10;
 const INT32_MAX = 2147483647;
 const INT32_MIN = -2147483648;
 
-const nanPayloadBuffer = Buffer.from('120000000000F87F', 'hex');
+const nanPayloadBuffer = Buffer.from("120000000000F87F", "hex");
 const nanPayloadDV = new DataView(
   nanPayloadBuffer.buffer,
   nanPayloadBuffer.byteOffset,
@@ -18,8 +18,8 @@ const nanPayloadDV = new DataView(
 );
 const nanPayloadDouble = nanPayloadDV.getFloat64(0, true);
 
-describe('serializing javascript numbers', () => {
-  it('should serialize a number that exceeds int32.max as a double', () => {
+describe("serializing javascript numbers", () => {
+  it("should serialize a number that exceeds int32.max as a double", () => {
     const document = { a: INT32_MAX + 1 };
     const bytes = BSON.serialize(document);
 
@@ -29,7 +29,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should serialize a number that negatively exceeds int32.min as a double', () => {
+  it("should serialize a number that negatively exceeds int32.min as a double", () => {
     const document = { a: INT32_MIN - 1 };
     const bytes = BSON.serialize(document);
 
@@ -39,7 +39,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should serialize a number that is exactly int32.max as int32', () => {
+  it("should serialize a number that is exactly int32.max as int32", () => {
     const document = { a: INT32_MAX };
     const bytes = BSON.serialize(document);
 
@@ -49,7 +49,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should serialize a number that is exactly int32.min as int32', () => {
+  it("should serialize a number that is exactly int32.min as int32", () => {
     const document = { a: INT32_MIN };
     const bytes = BSON.serialize(document);
 
@@ -59,7 +59,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should serialize a number that has a fractional component as double', () => {
+  it("should serialize a number that has a fractional component as double", () => {
     const document = { a: 2.3 };
     const bytes = BSON.serialize(document);
 
@@ -69,7 +69,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it.skip('should preserve -0.0 through round trip -- TODO(NODE-4335)', () => {
+  it.skip("should preserve -0.0 through round trip -- TODO(NODE-4335)", () => {
     // TODO(NODE-4335): -0 should be serialized as double
     const document = { a: -0.0 };
     const bytes = BSON.serialize(document);
@@ -81,7 +81,19 @@ describe('serializing javascript numbers', () => {
     expect(Object.is(returnedDocument.a, -0.0)).to.be.true;
   });
 
-  it('converts -0.0 to an int32 -- TODO(NODE-4335)', () => {
+  it("should preserve Double(-0.0) through round trip -- TODO(NODE-4335)", () => {
+    // TODO(NODE-4335): -0 should be serialized as double
+    const document = { a: new BSON.Double(-0.0) };
+    const bytes = BSON.serialize(document);
+
+    expect(bytes[4]).to.equal(BSON_DOUBLE_TYPE);
+
+    const returnedDocument = BSON.deserialize(bytes, { promoteValues: false });
+    expect(returnedDocument).to.deep.equal(document);
+    expect(Object.is(returnedDocument.a.valueOf(), -0.0)).to.be.true;
+  });
+
+  it("converts -0.0 to an int32 -- TODO(NODE-4335)", () => {
     // TODO(NODE-4335): -0 should be serialized as double
     // This test is demonstrating the behavior of -0 being serialized as an int32 something we do NOT want to unintentionally change, but may want to change in the future, which the above ticket serves to track.
     const document = { a: -0.0 };
@@ -90,11 +102,11 @@ describe('serializing javascript numbers', () => {
     expect(bytes[4]).to.equal(BSON_INT32_TYPE);
 
     const returnedDocument = BSON.deserialize(bytes);
-    expect(returnedDocument).to.have.property('a', 0);
+    expect(returnedDocument).to.have.property("a", 0);
     expect(Object.is(returnedDocument.a, -0.0)).to.be.false;
   });
 
-  it('should preserve NaN through round trip', () => {
+  it("should preserve NaN through round trip", () => {
     const document = { a: NaN };
     const bytes = BSON.serialize(document);
 
@@ -104,7 +116,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should preserve NaN with payload through round trip', () => {
+  it("should preserve NaN with payload through round trip", () => {
     const document = { a: nanPayloadDouble };
     const bytes = BSON.serialize(document);
 
@@ -115,7 +127,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should preserve Infinity through round trip', () => {
+  it("should preserve Infinity through round trip", () => {
     const document = { a: Infinity };
     const bytes = BSON.serialize(document);
 
@@ -125,7 +137,7 @@ describe('serializing javascript numbers', () => {
     expect(returnedDocument).to.deep.equal(document);
   });
 
-  it('should preserve -Infinity through round trip', () => {
+  it("should preserve -Infinity through round trip", () => {
     const document = { a: -Infinity };
     const bytes = BSON.serialize(document);
 
